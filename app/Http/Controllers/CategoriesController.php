5546b90bc3e7bf572020dafba01429878290f602
phamduc1823 <phamduc1823@gmail.com>
2023-10-05 16:19:44 +0700

    crud category done

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CategoriesRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    public function __construct(
+        public Category $category
+    )
+    {
+    }
+
+    public function list(){
+        try {
+            return responseApi($this->category::all(), true);
+        }catch (\Throwable $throwable)
+        {
+            return responseApi($throwable->getMessage(), false);
+        }
+    }
+
+    public function store(CategoriesRequest $request){
+        try {
+            if (!empty($request->validated())) {
+                $this->category::create($request->all());
+                return responseApi('Create successfully!', true);
+            }
+            return responseApi('false', false);
+        }catch (\Throwable $throwable)
+        {
+            return responseApi($throwable->getMessage(), false);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            if (!$this->category::find($id)) return responseApi('Category not found', false);
+            return responseApi($this->category::find($id), true);
+        }catch (\Throwable $throwable)
+        {
+            return responseApi($throwable->getMessage(), false);
+        }
+    }
+
+    public function update(CategoriesRequest $request, $id)
+    {
+        try {
+            if (!$this->category::find($id)) return responseApi('Category not found', false);
+            if (!empty($request->validated())) {
+                $category = $this->category::find($id);
+                $category->update($request->all());
+                return responseApi('Update successfully!', true);
+            }
+            return responseApi('false', false);
+        }catch (\Throwable $throwable)
+        {
+            return responseApi($throwable->getMessage(), false);
+        }
+    }
+
+    public function destroy($id){
+        try {
+            if (!$this->category::find($id)) return responseApi('Category not found', false);
+
+            $this->category::find($id)->delete();
+
+            return responseApi('Delete successfully!', true);
+        }catch (\Throwable $throwable)
+        {
+            return responseApi($throwable->getMessage(), false);
+        }
+    }
+}
---
+++ app/Http/Requests/CategoriesRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\TFailedValidation;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoriesRequest extends FormRequest
+{
+    use TFailedValidation;
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|unique:categories'
+//            unique:categories,category_name,".$id
+        ];
+    }
+
+     public function messages()
+     {
+         return [
+             "name.required" => "Không được để trống!",
+             "name.unique" => "Tên đã tồn tại!",
+         ];
+     }
+}

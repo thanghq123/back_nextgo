02804c63a82e5c0ee0f3d0228d0424b648e0715a
thanhnvph22301 <thanhnvph22301@fpt.edu.vn>
2023-11-13 22:17:37 +0700

    update api storage import

--- app/Http/Controllers/Tenant/VariationController.php
+++ app/Http/Controllers/Tenant/VariationController.php
@@ -10,7 +10,26 @@ class VariationController extends Controller
     public function getListVariation()
     {
         try {
-            return responseApi(Variation::with('variationQuantities')->get(),true);
+            $product = Variation::with(['variationQuantities', 'productName'])->get();
+            $result = $product->map(function($data){
+                return [
+                    'id' => $data->id,
+                    'product_id' => $data->product_id,
+                    'product_name_variation' => $data->productName[0]->name . ' - ' . $data->variation_name,
+                    'sku' => $data->sku,
+                    'barcode' => $data->barcode,
+                    'variation_name' => $data->variation_name,
+                    'display_name' => $data->display_name,
+                    'image' => $data->image,
+                    'price_import' => $data->price_import,
+                    'price_export' => $data->price_export,
+                    'status' => $data->status,
+                    'created_at' => $data->created_at,
+                    'updated_at' => $data->updated_at,
+                    'variation_quantities' => $data->variationQuantities
+                ];
+            });
+            return responseApi($result,true);
         }catch (\Throwable $throwable){
             return responseApi($throwable->getMessage());
         }

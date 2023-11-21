a0f9c4c0c2e955c4f1d6b35f31f6cdbfa391ac4b
mrbinh008 <binhvtx3@gmail.com>
2023-11-21 15:24:09 +0700

    update template login

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.auth')
-@section('title','Reset Password')
+@section('title','Đặt lại mật khẩu')
 @section('content')
     <form class="form w-100" novalidate="novalidate" id="kt_new_password_form" data-kt-redirect-url="{{route('login')}}" action="#">
         <!--begin::Heading-->
         @csrf
         <div class="text-center mb-10">
             <!--begin::Title-->
-            <h1 class="text-dark fw-bolder mb-3">Setup New Password</h1>
+            <h1 class="text-dark fw-bolder mb-3">Thiết lập mật khẩu mới</h1>
             <!--end::Title-->
             <!--begin::Link-->
-            <div class="text-gray-500 fw-semibold fs-6">Have you already reset the password ?
-                <a href="{{ route('login') }}" class="link-primary fw-bold">Sign in</a></div>
+            <div class="text-gray-500 fw-semibold fs-6">Bạn đã đặt lại mật khẩu chưa ?
+                <a href="{{ route('login') }}" class="link-primary fw-bold">Đăng nhập</a></div>
             <!--end::Link-->
         </div>
         <!--begin::Heading-->
@@ -20,7 +20,7 @@
             <div class="mb-1">
                 <!--begin::Input wrapper-->
                 <div class="position-relative mb-3">
-                    <input class="form-control bg-transparent" type="password" placeholder="Password" name="password" autocomplete="off" />
+                    <input class="form-control bg-transparent" type="password" placeholder="Mật khẩu" name="password" autocomplete="off" />
                     <span class="btn btn-sm btn-icon position-absolute translate-middle top-50 end-0 me-n2" data-kt-password-meter-control="visibility">
 													<i class="ki-duotone ki-eye-slash fs-2"></i>
 													<i class="ki-duotone ki-eye fs-2 d-none"></i>
@@ -38,14 +38,14 @@
             </div>
             <!--end::Wrapper-->
             <!--begin::Hint-->
-            <div class="text-muted">Use 8 or more characters with a mix of letters, numbers & symbols.</div>
+            <div class="text-muted">Mật khẩu phải từ 8 kí tự bao gồm cả chữ,số và kí tự </div>
             <!--end::Hint-->
         </div>
         <!--end::Input group=-->
         <!--end::Input group=-->
         <div class="fv-row mb-8">
             <!--begin::Repeat Password-->
-            <input type="password" placeholder="Repeat Password" name="confirm-password" autocomplete="off" class="form-control bg-transparent" />
+            <input type="password" placeholder="Nhập lại mật khẩu" name="confirm-password" autocomplete="off" class="form-control bg-transparent" />
             <!--end::Repeat Password-->
         </div>
         <!--end::Input group=-->
@@ -53,8 +53,8 @@
         <div class="fv-row mb-8">
             <label class="form-check form-check-inline">
                 <input class="form-check-input" type="checkbox" name="toc" value="1" />
-                <span class="form-check-label fw-semibold text-gray-700 fs-6 ms-1">I Agree &
-											<a href="#" class="ms-1 link-primary">Terms and conditions</a>.</span>
+                <span class="form-check-label fw-semibold text-gray-700 fs-6 ms-1">Tôi đồng ý &
+											<a href="#" class="ms-1 link-primary">Các điều khoản và điều kiện</a>.</span>
             </label>
         </div>
         <!--end::Input group=-->
@@ -62,10 +62,10 @@
         <div class="d-grid mb-10">
             <button type="button" id="kt_new_password_submit" class="btn btn-primary">
                 <!--begin::Indicator label-->
-                <span class="indicator-label">Submit</span>
+                <span class="indicator-label">Xác nhận</span>
                 <!--end::Indicator label-->
                 <!--begin::Indicator progress-->
-                <span class="indicator-progress">Please wait...
+                <span class="indicator-progress">Vui lòng đợi...
 											<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
                 <!--end::Indicator progress-->
             </button>
